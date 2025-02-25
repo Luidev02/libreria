8d373a8dc60a67a5c1345b4894e92d7cd35458c2
Luidev02 <luisfcp08@gmail.com>
2025-02-24 19:45:35 -0500

    mejoras

--- backend/src/config/apm.js
+++ backend/src/config/apm.js
@@ -3,12 +3,14 @@
 import apm from 'elastic-apm-node'
 
 apm.start({
-    serviceName: 'my-service-name',
-    secretToken: 'goK4GKt0elb6zgFLwm',
-    serverUrl: 'https://e7674d701ae1479b8050020d43c87a69.apm.us-central1.gcp.cloud.es.io:443',
-    environment: 'my-environment'
+    serviceName: process.env.APM_SERVICE_NAME,
+    secretToken: process.env.APM_SECRET_TOKEN,
+    serverUrl: process.env.APM_SERVER_URL,
+    environment: process.env.APM_ENVIRONMENT,
 
 })
 
 
 export default apm;
+
+
